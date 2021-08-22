c5d485f19427e73979a4cc19be2ccd7b1d7b7a02
usernamemadan <69414099+usernamemadan@users.noreply.github.com>
2021-08-23 00:28:12 +0530

    Update employee.java

--- employee.java
+++ employee.java
@@ -4,6 +4,16 @@ public class employee {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		if (isEmployeePresent()) {
+        			System.out.println("Employee is present");
+        		} else {
+        			System.out.println("Employee is absent");
+        		}
 	}
-
+	 public static boolean isEmployeePresent() {
+        	   Random rd = new Random();
+        	   return rd.nextBoolean();
+        }
+  
 }
