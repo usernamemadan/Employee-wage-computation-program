624a1fef734483385c1c6a0c29ed52b64689c9f3
Madan AR <madanar@106AMBBLR-Nithin-A-Y.local>
2021-08-22 23:43:24 +0530

    added program to display message - welcome to employee wage program- message

---
+++ employee/src/com/employee/Employee.java
@@ -0,0 +1,11 @@
+package com.employee;
+
+public class Employee {
+
+        public static void main(String[] args) {
+
+                System.out.println("Welcome to Employee Wage Computation Program");
+        }
+
+}
+
